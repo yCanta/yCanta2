ff960fe54d9837016d9d6d08ab8ad5fe7b7eca6f
Carl Hempel <cshempel@gmail.com>
2019-05-30 14:15:15 -0400

    Using list.js to generate the songlist and make it searchable in songbooks

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ var db = new PouchDB('yCanta');
 function saveSong(song_id) {
   var song_html = $('#song song');
   if(song_id == undefined) {
-    song_id = /*new Date().toISOString()*/ 's-' + song_html.find('stitle').text();
+    song_id = 's-' /*new Date().toISOString()*/  + song_html.find('stitle').text();
   }
   
   var song = {
@@ -34,27 +34,59 @@ function loadSong(song_id) {
 
 function loadSongbook(songbook_id) {
   function buildSongbookList (result) {
+    var options = {    
+      valueNames: [
+        { data: ['song-id'] },
+        { data: ['song-title'] },
+        { data: ['song-authors'] },
+        { data: ['song-scripture'] },
+        { data: ['song-introduction'] },
+        { data: ['song-key'] },
+        { data: ['song-categories'] },
+        { data: ['song-cclis'] },
+        { data: ['song-content'] },
+        { data: ['song-copyright'] },
+        { name: 'link', attr: 'href'},
+        'name'
+      ],
+      item: 'song-item-template'
+    };
+    var values = [];   
+    result.rows.map(function (row) {
+      if(window.songbook_list != undefined){
+        if(window.songbook_list.get(row.doc._id)){
+          console.log("Skipping it!");
+          return
+        }
+        else {
+          console.log("We've got a new one!");
+        }
+      }
+      values.push(
+        { 'song-id': row.doc._id,
+          'song-title':        't:' + row.doc.title,
+          'song-authors':      'a:' + (row.doc.authors != '' ? row.doc.authors : '!a'),
+          'song-scripture':    's:' + row.doc.scripture,
+          'song-introduction': 'i:' + row.doc.introduction,
+          'song-key':          'k:' + row.doc.key,
+          'song-categories':   'c:' + row.doc.categories,
+          'song-copyright':    'c:' + (row.doc.copyright ? row.doc.copyright : '!c'),
+          'song-cclis': ((row.doc.cclis!='') ? 'cclis' : '!cclis'),
+          'song-content': row.doc.content,
+          'link': '#'+songbook_id+'&'+row.doc._id,
+          'name': row.doc.title
+      });
+    });
+    window.songbook_list = new List('songbook_content', options, values);
+    /*
     var songbook = document.createDocumentFragment();
       var title = document.createElement('h3');
       title.innerHTML = 'todosCantas';
       songbook.appendChild(title);
 
-      var songs_list = document.createElement('ol');
-      result.rows.map(function (row) {
-        var song = document.createElement('li');
-        var link = document.createElement('a');
-        link.innerHTML = row.doc.title;
-        link.href = '#' + songbook_id + '&' + row.doc._id;
-        link.dataset.rev = row.doc._rev;
-        song.appendChild(link);
-        songs_list.appendChild(song);
-        return;
-      });
-      songbook.appendChild(songs_list);
-
       var songbook_content = document.getElementById("songbook_content");
       songbook_content.innerHTML = '';
-      songbook_content.append(songbook);
+      songbook_content.append(songbook);*/
     return 
   }
   if(songbook_id == undefined) {
@@ -73,6 +105,7 @@ function loadSongbook(songbook_id) {
   else {
     //we need to load the songbook - mostly same as above.
   }
+  $('#songbook_title').text('todosCantas');  // Need to change for other songbooks.
   $('body').attr('class','songList');
 }
 
