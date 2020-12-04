1f88e4cce0e7e496415378f7e420514afd07ae60
Carl Hempel <cshempel@gmail.com>
2020-12-04 15:42:45 -0500

    Trying something... to bug hunt on mobile

--- lib.js
+++ lib.js
@@ -1658,3 +1658,18 @@ $(function() {
   });
 });
 
+window.onerror = function (msg, url, lineNo, columnNo, error) {
+  var string = msg.toLowerCase();
+  var substring = "script error";
+
+  var message = [
+    'Message: ' + msg,
+    'URL: ' + url,
+    'Line: ' + lineNo,
+    'Column: ' + columnNo,
+    'Error object: ' + JSON.stringify(error)
+  ].join(' - ');
+
+  alert(message);
+  return false;
+};
