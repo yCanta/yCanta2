1438010a98e31b5e53b0b6722856c87783d7d18a
Carl Hempel <cshempel@gmail.com>
2020-12-16 14:45:39 -0500

    Reformatted jquery.myTransposer.js file and tweaked 2html styles slightly

--- jquery.myTransposer.js
+++ jquery.myTransposer.js
@@ -10,12 +10,9 @@
  * Modified by: Carl Hempelk 11/12/12
  */
 (function($) {
-
   $.fn.transpose = function(options) {
     var opts = $.extend({}, $.fn.transpose.defaults, options);
-    
     var currentKey = null;
-    
     var keys = [
       { name: 'Ab',  value: 0,   type: 'F' },
       { name: 'A',   value: 1,   type: 'N' },
@@ -35,92 +32,86 @@
       { name: 'G',   value: 11,  type: 'N' },
       { name: 'G#',  value: 0,   type: 'S' }
     ];
-  
     var getKeyByName = function (name) {
-        if (name.charAt(name.length-1) == "m") {
-          name = name.substring(0, name.length-1);
+      if (name.charAt(name.length-1) == "m") {
+        name = name.substring(0, name.length-1);
+      }
+      for (var i = 0; i < keys.length; i++) {
+        if (name == keys[i].name) {
+            return keys[i];
         }
-        for (var i = 0; i < keys.length; i++) {
-            if (name == keys[i].name) {
-                return keys[i];
-            }
-        }
+      }
     };
-
     var getChordRoot = function (input) {
-        if (input.length > 1 && (input.charAt(1) == "b" || input.charAt(1) == "#"))
-            return input.substr(0, 2);
-        else
-            return input.substr(0, 1);
+      if (input.length > 1 && (input.charAt(1) == "b" || input.charAt(1) == "#"))
+        return input.substr(0, 2);
+      else
+        return input.substr(0, 1);
     };
-
     var getNewKey = function (oldKey, delta, targetKey) {
-        var keyValue = getKeyByName(oldKey).value + delta;
+      var keyValue = getKeyByName(oldKey).value + delta;
 
-        if (keyValue > 11) {
-            keyValue -= 12;
-        } else if (keyValue < 0) {
-            keyValue += 12;
-        }
-        
-        var i=0;
-        if (keyValue == 0 || keyValue == 2 || keyValue == 5 || keyValue == 7 || keyValue == 10) {
-            // Return the Flat or Sharp Key
-            switch(targetKey.name) {
-              case "A":
-              case "A#":
-              case "B":
-              case "C":
-              case "C#":
-              case "D":
-              case "D#":
-              case "E":
-              case "F#":
-              case "G":
-              case "G#":
-                  for (;i<keys.length;i++) {
-                    if (keys[i].value == keyValue && keys[i].type == "S") {
-                      return keys[i];
-                    }
-                  }
-              default:
-                  for (;i<keys.length;i++) {
-                    if (keys[i].value == keyValue && keys[i].type == "F") {
-                      return keys[i];
-                    }
-                  }
-            }
-        }
-        else {
-            // Return the Natural Key
+      if (keyValue > 11) {
+        keyValue -= 12;
+      } else if (keyValue < 0) {
+        keyValue += 12;
+      }
+      
+      var i=0;
+      if (keyValue == 0 || keyValue == 2 || keyValue == 5 || keyValue == 7 || keyValue == 10) {
+        // Return the Flat or Sharp Key
+        switch(targetKey.name) {
+          case "A":
+          case "A#":
+          case "B":
+          case "C":
+          case "C#":
+          case "D":
+          case "D#":
+          case "E":
+          case "F#":
+          case "G":
+          case "G#":
             for (;i<keys.length;i++) {
-              if (keys[i].value == keyValue) {
+              if (keys[i].value == keyValue && keys[i].type == "S") {
+                return keys[i];
+              }
+            }
+          default:
+            for (;i<keys.length;i++) {
+              if (keys[i].value == keyValue && keys[i].type == "F") {
                 return keys[i];
               }
             }
         }
-    };
-
-    var getChordType = function (key) {
-        switch (key.charAt(key.length - 1)) {
-            case "b":
-                return "F";
-            case "#":
-                return "S";
-            default:
-              return "N";
+      }
+      else {
+        // Return the Natural Key
+        for (;i<keys.length;i++) {
+          if (keys[i].value == keyValue) {
+            return keys[i];
+          }
         }
+      }
     };
-
+    var getChordType = function (key) {
+      switch (key.charAt(key.length - 1)) {
+        case "b":
+          return "F";
+        case "#":
+          return "S";
+        default:
+          return "N";
+      }
+    };
     var getDelta = function (oldIndex, newIndex) {
-        if (oldIndex > newIndex)
-            return 0 - (oldIndex - newIndex);
-        else if (oldIndex < newIndex)
-            return 0 + (newIndex - oldIndex);
-        else
-            return 0;
+      if (oldIndex > newIndex)
+        return 0 - (oldIndex - newIndex);
+      else if (oldIndex < newIndex)
+        return 0 + (newIndex - oldIndex);
+      else
+        return 0;
     };
-
     var transposeSong = function (target, key) {
       var newKey = getKeyByName(key);
       currentKey = getKeyByName($('key').text()) || newKey;
@@ -154,110 +145,97 @@
       currentKey = newKey;
     };
     var transposeLine = function (line, delta, targetKey) {
-       var newline = []
-       var diff = 0
-       var line = line.split(" ")
-       for (var i = 0; i < line.length; i++) {
-         if (line[i] != "") {
-           var oldChordRoot = getChordRoot(line[i]);
-           var newChordRoot = getNewKey(oldChordRoot, delta, targetKey);
-           var newChord = newChordRoot.name + line[i].substr(oldChordRoot.length);
-           newline.push(newChord);
-           diff = line[i].length - newChord.length
-         }
-         else {
-           if (diff < 0){
-             diff = diff + 1
-           }
-           else {
-             newline.push(line[i])
-           }
-         }
-         for (; diff > 0; diff = diff -1) {
-           newline.push("");
-         }
-       }
-       return newline.join(" ")
-    };
-
-    var transposeChord = function (selector, delta, targetKey) {
-        var el = $(selector);
-        var oldChord = el.text();
-        var oldChordRoot = getChordRoot(oldChord);
-        var newChordRoot = getNewKey(oldChordRoot, delta, targetKey);
-        var newChord = newChordRoot.name + oldChord.substr(oldChordRoot.length);
-        el.text(newChord);
-    };
-
-    var getNewWhiteSpaceLength = function (a, b, c) {
-        if (a > b)
-            return (c + (a - b));
-        else if (a < b)
-            return (c - (b - a));
-        else
-            return c;
-    };
-
-    var makeString = function (s, repeat) {
-        var o = [];
-        for (var i = 0; i < repeat; i++) o.push(s);
-        return o.join("");
-    }
-    
-    
-    var isChordLine = function (input) {
-        var tokens = input.replace(/\s+/, " ").split(" ");
-
-        // Try to find tokens that aren't chords
-        // if we find one we know that this line is not a 'chord' line.
-        for (var i = 0; i < tokens.length; i++) {
-            if (!$.trim(tokens[i]).length == 0 && !tokens[i].match(opts.chordRegex))
-                return false;
+      var newline = []
+      var diff = 0
+      var line = line.split(" ")
+      for (var i = 0; i < line.length; i++) {
+        if (line[i] != "") {
+          var oldChordRoot = getChordRoot(line[i]);
+          var newChordRoot = getNewKey(oldChordRoot, delta, targetKey);
+          var newChord = newChordRoot.name + line[i].substr(oldChordRoot.length);
+          newline.push(newChord);
+          diff = line[i].length - newChord.length
         }
-        return true;
+        else {
+          if (diff < 0){
+            diff = diff + 1
+          }
+          else {
+            newline.push(line[i])
+          }
+        }
+        for(; diff > 0; diff = diff -1) {
+          newline.push("");
+        }
+      }
+      return newline.join(" ")
     };
-    
+    var transposeChord = function (selector, delta, targetKey) {
+      var el = $(selector);
+      var oldChord = el.text();
+      var oldChordRoot = getChordRoot(oldChord);
+      var newChordRoot = getNewKey(oldChordRoot, delta, targetKey);
+      var newChord = newChordRoot.name + oldChord.substr(oldChordRoot.length);
+      el.text(newChord);
+    };
+    var getNewWhiteSpaceLength = function (a, b, c) {
+      if (a > b)
+        return (c + (a - b));
+      else if (a < b)
+        return (c - (b - a));
+      else
+        return c;
+    };
+    var makeString = function (s, repeat) {
+      var o = [];
+      for (var i = 0; i < repeat; i++) o.push(s);
+      return o.join("");
+    }
+    var isChordLine = function (input) {
+      var tokens = input.replace(/\s+/, " ").split(" ");
+      // Try to find tokens that aren't chords
+      // if we find one we know that this line is not a 'chord' line.
+      for (var i = 0; i < tokens.length; i++) {
+        if (!$.trim(tokens[i]).length == 0 && !tokens[i].match(opts.chordRegex))
+          return false;
+      }
+      return true;
+    };
     var wrapChords = function (input) {
-        return input.replace(opts.chordReplaceRegex, "<span class='c'>$1</span>");
+      return input.replace(opts.chordReplaceRegex, "<span class='c'>$1</span>");
     };
-    
     
     return $(this).each(function() {
-    
       var startKey = $('key').text();
       if (!startKey || $.trim(startKey) == "") {
         startKey = "" 
       }
-
-      
       currentKey = getKeyByName(startKey) || "";
 
       // Build tranpose links ===========================================
       var keyLinks = [];
       $(keys).each(function(i, key) {
-          if (currentKey.name == key.name)
-              keyLinks.push("<a tabindex='0' id='"+key.name.replace('#','_')+"' class='selected default'>" + key.name + "</a>");
-          else
-              keyLinks.push("<a tabindex='0' id='"+key.name.replace('#','_')+"'>" + key.name + "</a>");
+        if (currentKey.name == key.name)
+          keyLinks.push("<a tabindex='0' id='"+key.name.replace('#','_')+"' class='selected default'>" + key.name + "</a>");
+        else
+          keyLinks.push("<a tabindex='0' id='"+key.name.replace('#','_')+"'>" + key.name + "</a>");
       });
-
 
       var $this = $(this);
       var keysHtml = $("<div class='transpose-keys'></div>");
       keysHtml.html(keyLinks.join(""));
       $("a", keysHtml).click(function(e) {
-          e.preventDefault();
-          transposeSong($this, $(this).text());
-          $(".transpose-keys a").removeClass("selected");
-          $(this).addClass("selected");
-          $('key').text($(this).text());
-          return false;
+        e.preventDefault();
+        transposeSong($this, $(this).text());
+        $(".transpose-keys a").removeClass("selected");
+        $(this).addClass("selected");
+        $('key').text($(this).text());
+        return false;
       });
       
       $(this).after(keysHtml);
     });
   };
-
 
   $.fn.transpose.defaults = {
     chordRegex: /^[A-G][b\#]?(2|5|6|7|9|11|13|6\/9|7\-5|7\-9|7\#5|7\#9|7\+5|7\+9|7b5|7b9|7sus2|7sus4|add2|add4|add9|aug|dim|dim7|m\/maj7|m6|m7|m7b5|m9|m11|m13|maj7|maj9|maj11|maj13|mb5|m|sus|sus2|sus4)*(\/[A-G][b\#]*)*$/,
