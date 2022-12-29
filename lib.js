b1f2598f384a2ccfdb64dd35a62067cbbd3b20b0
Carl Hempel <cshempel@gmail.com>
2022-12-28 20:57:19 -0500

    #100 Fixed!

--- lib.js
+++ lib.js
@@ -1176,7 +1176,7 @@ function editSong() {
 
   $('#song authors').html($('#song author').map(function(){return $(this).text();}).get().join(', '));
   $('#song categories').html($('#song cat').map(function(){return $(this).text();}).get().join(', '));
-  $('#song scripture_ref').html($('#song cat').map(function(){return $(this).text();}).get().join(', '));
+  $('#song scripture_ref').html($('#song scrip_ref').map(function(){return $(this).text();}).get().join(', '));
   $('#song').first('.subcolumn').find('stitle .title_link,authors,scripture_ref,introduction,key,chunk,copyright,cclis').toTextarea({
     allowHTML: false,//allow HTML formatting with CTRL+b, CTRL+i, etc.
     allowImg: false,//allow drag and drop images
